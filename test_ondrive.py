b7879b2ecb66f5bdf33bb20a258aa38fbade9dde
Paulo Silva <pangerspol@gmail.com>
2025-01-07 15:11:12 -0500

    Added test for list_drive_contents

--- test_ondrive.py
+++ test_ondrive.py
@@ -3,8 +3,15 @@ from core.onedrive import OneDriveManager
 manager = OneDriveManager()
 manager.authenticate()
 
+# Create a folder
 #result = manager.create_folder("Test Folder")
 #print("Folder creation result:", result)
 
-drives = manager.list_drives()
-print("List of drives:", drives)
+# List drives available
+#drives = manager.list_drives()
+#print("List of drives:", drives)
+
+# List the contents of the drive
+drive_id = "b!Sm5Rdoh1bUq94YPabsGWGuRxlSNtNf5JiFajd7jPnp67gLodnSjvQ7y2xdmZql36"
+contents = manager.list_drive_contents(drive_id)
+print("Drive Contents:", contents)
