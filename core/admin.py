daa512787b270705c1edc33f5dcb9c281766a060
Paulo Silva <pangerspol@gmail.com>
2025-01-06 22:13:00 -0500

    Registered Request Model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Request
 
 # Register your models here.
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('case_number', 'client_name', 'paralegal', 'office', 'created_at')
