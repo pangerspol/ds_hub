64463262f26af2f2bb91d2931136fcf5ef8562e2
Paulo Silva <pangerspol@gmail.com>
2025-01-06 22:08:46 -0500

    Defined Request Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Request(models.Model):
+    case_number = models.CharField(max_length=50)
+    client_name = models.CharField(max_length=100)
+    paralegal = models.CharField(max_length=100)
+    office = models.CharField(max_length=50, choices=[('Hartford', 'Hartford'), ('Waterbury', 'Waterbury')])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.case_number} - {self.client_name}"
